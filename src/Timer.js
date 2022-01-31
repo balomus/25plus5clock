7f0e3af7d99bc2cd6a52ab693d4cf341c4b15818
Stephen Mercier <sjmercier@wcu.edu>
2022-01-31 10:37:26 -0500

    Some initial elements added to page.

--- src/Timer.js
+++ src/Timer.js
@@ -1,9 +1,21 @@
 const Timer = (props) => {
     return ( 
         <div>
-            Session
-            <div>
-                {props.clock}
+            <div id="timer-label">
+                Session
+            </div>
+
+            <div id="time-left">
+                {props.clock} : 00
+            </div>
+
+            <div id="session-controls">
+                <button id="start_stop">
+                    Play/Pause
+                </button>
+                <button id="reset">
+                    Reset
+                </button>
             </div>
         </div>
      );
--- src/TimerControl.js
+++ src/TimerControl.js
@@ -1,21 +1,33 @@
 const TimerControl = (props) => {
+
     return ( 
         <div>
             <div id="break-label">
                 Break Length
                 <br />
-                <button>
-                    Up arrow 
+                <button id="break-decrement">
+                    Down arrow 
                 </button>
-                {props.breakTime} 
-                <button>
-                    Down arrow
+                <div id="break-length">
+                    {props.breakTime} 
+                </div>
+                <button id="break-increment">
+                    Up arrow
                 </button>
             </div>
+            
             <div id="session-label">
                 Session Length
                 <br />
-                Up arrow {props.sessionTime} Down arrow
+                <button id="session-decrement">
+                    Down arrow
+                </button>
+                <div id="session-length">
+                    {props.sessionTime}
+                </div>
+                <button id="session-increment">
+                    Up arrow
+                </button>
             </div>
         </div>
      );
